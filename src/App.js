92127ac6f68ee2dd8b5b25714693efb0c616f643
Maxwell Lasky <mlasky46@gmail.com>
2017-10-03 15:07:19 -0600

    stupid readme

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends Component {
     fetch('https://api.myjson.com/bins/ygo8t')
       .then((results) => results.json())
       .then((results) => {
+        let logged = false
         this.setState({
           results: results.map((result, i) => {
             const copy = {...result}
@@ -26,6 +27,10 @@ class App extends Component {
               _copy.superHeavy = [...results];
               return _copy;
             })
+            if (!logged) {
+              console.log(JSON.stringify(copy))
+            }
+            logged = true
             return copy;
           }),
           loading: false
